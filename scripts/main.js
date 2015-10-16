1186f61ad7332529578812bb7d6c024334945099
7blink <7blinkcreations@gmail.com>
2015-10-15 20:39:03 -0400

    added tags and posts

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+
+
+  function toggle_visibility(id) {
+     var tag = document.getElementById(id);
+     if(tag.style.display == 'block')
+        tag.style.display = 'none';
+     else
+        tag.style.display = 'block';
+  }
